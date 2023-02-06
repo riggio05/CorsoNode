d4170eed96869b35fbe7c3c8234d940b851e5d66
riggio05 <simone.riggio@itiszuccante.edu.it>
2023-02-06 17:15:00 +0100

    node

--- GiocoMemoriaVisiva/scripts.js
+++ GiocoMemoriaVisiva/scripts.js
@@ -3,11 +3,39 @@ let s = 10;
 
 let memory = [];
 let tableData = [];
-let colors =  
+let colors =  [
+    "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 255, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(100, 100, 100)",
+    "rgb(100, 0, 0)",
+    "rgb(0, 100, 0)",
+    "rgb(0, 0, 100)",
+]
 
 table();
 
 function table() {
+    for(let i = 0; i < size; ++i) {
+        let tr = document.createElement("tr");
+        for(let j = 0; j < size; ++j) {
+            memory[i] = [];
+            tableData[i] = [];
+            let td = document.createElement("td");
+            td.innerHTML = "AA";
+            td.style.backgroundColor = ;
+            memory[i][j] = "rgb(10, 10, 10)"
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    
+    }
+}
+
+function showMe() {
     for(let i = 0; i < size; ++i) {
         let tr = document.createElement("tr");
         for(let j = 0; j < size; ++j) {
